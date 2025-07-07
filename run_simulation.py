c720afc2e7b898314686ec0b396025856985fb73
txpowerranger <mikelund23@gmail.com>
2025-07-06 18:14:47 -0600

    Update run_simulation.py
    Included endurance simulation, and point calculations

--- run_simulation.py
+++ run_simulation.py
@@ -26,14 +26,25 @@ time = [0]
 
 
 #---Run Simulation---
-human_factor=1
-states1, velocities1, time1, laptime1, sector_times = run_simulation_car(
-    state, states, velocities, time, track, vehicle, human_factor
-) 
+human_factor=1 #the minimum percent of ideal. Currently 100% or perfectly ideal
+if track.track_type!= 'endur':
+    states1, velocities1, time1, laptime1, sector_times = run_simulation_car(
+        state, states, velocities, time, track, vehicle, human_factor)
+
+    points,max_points,place=calculate_result(track,track_config,extracted_tables,laptime1)
+    plot_simulation_results(track,velocities1,sector_boundaries,sector_times,points,max_points,place)
+
+else:
+    states1,velocities1,time1,laptime1,sector_times= simulate_endurance(
+        inital_state,states,velocities,time,track,vehicle,human_factor)
+
+    points,max_points,place = calculate_result(track,track_config,extracted_tables,np.sum(laptime1))
+
+    print(f'Points Scored: {points:.2f}/{max_points}')
+    print(f'Expected Place: {place}')
 
 
-#---Plotting Results---
-plot_simulation_results(track.s,velocities1,track.x,track.y,sector_boundaries,sector_times)
+#%%---Comparing Results---
 '''
 state = np.array([0.0, 0.0, 0.0])  # Initial state [vx, vy, r (yaw rate)]
 states = [state]
